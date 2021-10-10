0a481d3c85f6484444b5f4d0e7917e5b02d8620f
rajnishmalhi4844@gmail.com <rajnishmalhi4844@gmail.com>
2021-10-11 01:13:05 +0300

    Add project files.

---
+++ OnlineAppointment_Rajnish/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineAppointment_Rajnish.Startup))]
+namespace OnlineAppointment_Rajnish
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
